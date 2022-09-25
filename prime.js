5679c0a07591cfb4881bbe45aeb37d422ade34cf
Preetykumari06 <preetysingh46231@gmail.com>
2022-09-25 13:59:04 +0530

    code fixed by preety

--- prime.js
+++ prime.js
@@ -1,9 +1,19 @@
-function prime(num){
- for(let i=0; i<=num; i++){
-  if(num%i){
-    return true;
+function prime(number){
+ let factors=0;
+ for(let i=1; i<=number; i++){
+  if(number%i==0){
+    factors++;
    }
- }
+} 
+  if(factors==2){
+    return true;
+  } 
    return false;
 }
-  prime(17);
+
+let ans=prime(17);
+  if(ans==true){
+    console.log("Prime");
+  } else {
+    console.log("Not prime");
+}
